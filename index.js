f9427c4fa91b0f786f7fc0b404f913b2e31cad1e
Tanjid Karim <you@example.com>
2025-02-21 17:42:46 +0600

    reordered implemented

--- index.js
+++ index.js
@@ -6,10 +6,9 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-const { MongoClient, ServerApiVersion, ObjectId, Timestamp } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pcjdk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -20,31 +19,35 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    // // Send a ping to confirm a successful connection
-    // await client.db('admin').command({ ping: 1 });
-    console.log('Pinged your deployment. You successfully connected to MongoDB!');
+    // await client.connect()
+    // console.log("Connected to MongoDB!")
     const taskCollection = client.db('Tasks').collection('Task');
-    //task fetch
+
+    // Fetch tasks
     app.get('/tasks', async (req, res) => {
-      const result = await taskCollection.find().toArray();
+      const result = await taskCollection.find().sort({ order: 1 }).toArray();
       res.send(result);
     });
-    //task add
+
+    // Add task
     app.post('/tasks', async (req, res) => {
       const data = req.body;
-      const result = await taskCollection.insertOne(data);
+      const maxOrderTask = await taskCollection.findOne({ category: data.category }, { sort: { order: -1 } });
+      const order = maxOrderTask ? maxOrderTask.order + 1 : 0;
+      const taskWithOrder = { ...data, order };
+      const result = await taskCollection.insertOne(taskWithOrder);
       res.send(result);
     });
-    //delete task
+
+    // Delete task
     app.delete('/tasks/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await taskCollection.deleteOne(query);
       res.send(result);
     });
-    //update task
+
+    // Update task
     app.put('/tasks/:id', async (req, res) => {
       const id = req.params.id;
       const data = req.body;
@@ -60,16 +63,30 @@ async function run() {
       const result = await taskCollection.updateOne(query, updatedData);
       res.send(result);
     });
+
+    // Reorder tasks
+    app.post('/tasks/reorder', async (req, res) => {
+      const { category, taskIds } = req.body;
+      const bulkOps = taskIds.map((id, index) => ({
+        updateOne: {
+          filter: { _id: new ObjectId(id) },
+          update: { $set: { order: index, category } },
+        },
+      }));
+      const result = await taskCollection.bulkWrite(bulkOps);
+      res.send(result);
+    });
   } finally {
-    // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
+
 run().catch(console.dir);
 
 app.get('/', async (req, res) => {
   res.send('Tasks are Cooking');
 });
+
 app.listen(PORT, () => {
-  console.log(`Ports are running on ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
